efb4e397aaf710ddf61ccd48b368451254342f38
lijunwei <ljw532344863@sina.com>
2022-04-13 22:51:12 +0800

    Migrate from custom-omz-plugins.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# omz-thought
+**Migrate from [custom-omz-plugins](https://github.com/liijunwei/custom-omz-plugins)**
+
+# omz-thought: Add thought to blog
+
+```bash
+thought "hello world"
+```
+
+就一个用处: 使用命令行随时记录所思所想...
+
++ TODO 减少 或者 取消这种碎碎念, 把想法转为小的文章
+
---
+++ omz-thought.plugin.zsh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function thought() {
+  local msg="$*"
+  [[ -z "$msg" ]] && msg="Simple, Elegant, Flexible, Nothing..."
+
+  local content="$(date "+%Y-%m-%d %H:%M:%S") $msg"
+  local thoughts_filepath="$HOME/OuterGitRepo/blog-gallary/source/thoughts/index.md"
+
+  cd $HOME/OuterGitRepo/blog-gallary
+
+  if [ "$1" = "--edit" -o "$1" = "-e" ]; then
+    vi source/thoughts/index.md
+    return 0
+  fi
+
+  if [ "$1" = "--interactive" -o "$1" = "-i" ]; then
+    echo -n "Please type in your thoughts(ctrl+c to interrupt): "
+    read msg
+    if [ $? != 0 ]; then return $?; fi
+
+    local content="$(date "+%Y-%m-%d %H:%M:%S") $msg"
+  fi
+
+  echo $content
+  # macos use gsed, not builtin sed
+  gsed -i "6i - ${content}" $thoughts_filepath
+  gsed -i "6G" $thoughts_filepath
+
+  git add . &&
+  git commit -m "Commit manually." --quiet &&
+  git push --quiet &&
+  hexo clean  >/dev/null &&
+  hexo deploy >/dev/null &&
+  ssh webuser@xiaoli "cd /srv/www/blog-gallary &&
+                      git fetch         &&
+                      git checkout main &&
+                      git reset --hard HEAD@{u}"
+
+  cd -
+  echo
+  echo "open $BLOG_DOMAIN/thoughts"
+}
